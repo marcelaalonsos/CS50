a9b09e1cff13ba947f02341db4ea15393050ea8a
Marcela Alonso <MARCELA.ALONSO.S@GMAIL.COM>
2023-05-15 11:05:10 -0400

    week 1 notes and lecture done

--- WEEK 1/calculator.c
+++ WEEK 1/calculator.c
@@ -10,10 +10,37 @@
 //   int y = get_int("y: ");
 
 //   //perform addition
-//   printf("%i\n", x + y); //the %i
+//   printf("%i\n", x + y); //the %i prints integers
 // }
 
 //using long data type
+// int main(void)
+// {
+//   //prompt user for x
+//   long x = get_long("x: ");
+
+//   //prompt user for y
+//   long y = get_long("y: ");
+
+//   //perform addition
+//   printf("%li\n", x + y); //the %li prints long values
+// }
+
+//dividing ints and storing them in float variable
+// int main(void)
+// {
+//   //prompt user for x
+//   long x = get_long("x: ");
+
+//   //prompt user for y
+//   long y = get_long("y: ");
+
+//   float z = (float) x / (float) y; //type casting x and y as floats - converting them to another data type
+//   //perform addition
+//   printf("%f\n", z); //the %f prints floats
+// }
+
+//dividing ints and storing them as doubles for more precision
 int main(void)
 {
   //prompt user for x
@@ -22,6 +49,7 @@ int main(void)
   //prompt user for y
   long y = get_long("y: ");
 
+  double z = (double) x / (double) y; //type casting x and y as doubles - converting them to another data type
   //perform addition
-  printf("%li\n", x + y); //the %i
+  printf("%f\n", z); //the %f prints floats
 }
