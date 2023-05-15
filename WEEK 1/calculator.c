fbb33186ad4a5f8091bb4fd33c6b66b94207fbf8
Marcela Alonso <MARCELA.ALONSO.S@GMAIL.COM>
2023-05-15 10:55:42 -0400

    week 1 notes and lecture done

--- WEEK 1/calculator.c
+++ WEEK 1/calculator.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include "cs50.h"
 
+// int main(void)
+// {
+//   //prompt user for x
+//   int x = get_int("x: ");
+
+//   //prompt user for y
+//   int y = get_int("y: ");
+
+//   //perform addition
+//   printf("%i\n", x + y); //the %i
+// }
+
+//using long data type
 int main(void)
 {
   //prompt user for x
-  int x = get_int("x: ");
+  long x = get_long("x: ");
 
   //prompt user for y
-  int y = get_int("y: ");
+  long y = get_long("y: ");
 
   //perform addition
-  printf("%i\n", x + y); //the %i
+  printf("%li\n", x + y); //the %i
 }
